591171adc552084aad2b27d0cf1cc9a842bc5b81
thachtb <thach.bao@incognito.org>
2021-01-20 17:09:41 +0700

    Created multisig address and spent utxos.

--- relaying/btc/address_test.go
+++ relaying/btc/address_test.go
@@ -1,7 +1,16 @@
 package btcrelaying
 
 import (
+	"bytes"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/btcsuite/btcd/chaincfg"
@@ -127,13 +136,13 @@ func TestBTCMainnetAddress(t *testing.T) {
 		isValid bool
 	}
 	testcases := []BTCMainnetAddressTestCases{
-		{"bc1qq7ndvtvyzcea44ps6d4nt3plk02ghpsha0t55y", true},                     // AddressWitnessPubKeyHash
-		{"1KN7N34ZUd1HyXgqcJpeGrooQcLf2L4xFC", true},                             // AddressPubKeyHash
-		{"3EktnHQD7RiAE6uzMj2ZifT9YgRrkSgzQX", true},                             // AddressScriptHash
-		{"bc1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3qccfmv3", true}, // AddressWitnessScriptHash
-		{"37ExSZhkPhSwmzdjbeznK529vvrgS3qsJW", true},                             // legacy - AddressScriptHash
-		{"3EtBoGNHBd1zCH2A5WTExJrizB7TiBw4ci", true},                             // p2sh-segwit -- AddressScriptHash
-		{"bc1qpx5p30dcfxqpz5sxemv30ky34lf20jwe7nl95exqcphjvxxehalql9mmrd", true}, // bech32 -- AddressWitnessScriptHash
+		{"bc1qq7ndvtvyzcea44ps6d4nt3plk02ghpsha0t55y", true},                      // AddressWitnessPubKeyHash
+		{"1KN7N34ZUd1HyXgqcJpeGrooQcLf2L4xFC", true},                              // AddressPubKeyHash
+		{"3EktnHQD7RiAE6uzMj2ZifT9YgRrkSgzQX", true},                              // AddressScriptHash
+		{"bc1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3qccfmv3", true},  // AddressWitnessScriptHash
+		{"37ExSZhkPhSwmzdjbeznK529vvrgS3qsJW", true},                              // legacy - AddressScriptHash
+		{"3EtBoGNHBd1zCH2A5WTExJrizB7TiBw4ci", true},                              // p2sh-segwit -- AddressScriptHash
+		{"bc1qpx5p30dcfxqpz5sxemv30ky34lf20jwe7nl95exqcphjvxxehalql9mmrd", true},  // bech32 -- AddressWitnessScriptHash
 		{"tb1qwtlr3cmn0kg3h6passf7wktmy7596p7swpmxdz6nsp6pmvhzg3eq93qvfz", false}, // bech32 -- AddressWitnessScriptHash - testnet
 	}
 
@@ -196,13 +205,13 @@ func TestBTCTestnetAddress(t *testing.T) {
 		isValid bool
 	}
 	testcases := []BTCTestnetAddressTestCases{
-		{"tb1qw508d6qejxtdg4y5r3zarvary0c5xw7kxpjzsx", true},                     // AddressWitnessPubKeyHash
-		{"mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn", true},                             // AddressPubKeyHash
-		{"2MzQwSSnBHWHqSAqtTVQ6v47XtaisrJa1Vc", true},                            // AddressScriptHash
-		{"tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7", true}, // AddressWitnessScriptHash
-		{"2Mx7sVozbZZXPiqsTRWLnZ7bC7vGUEEwX6g", true},                            // legacy - AddressScriptHash
-		{"2MuiiTCHGtQ3MMFhAQ3kFGsJ6N9K89itPcw", true},                            // p2sh-segwit -- AddressScriptHash
-		{"tb1qwtlr3cmn0kg3h6passf7wktmy7596p7swpmxdz6nsp6pmvhzg3eq93qvfz", true}, // bech32 -- AddressWitnessScriptHash
+		{"tb1qw508d6qejxtdg4y5r3zarvary0c5xw7kxpjzsx", true},                      // AddressWitnessPubKeyHash
+		{"mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn", true},                              // AddressPubKeyHash
+		{"2MzQwSSnBHWHqSAqtTVQ6v47XtaisrJa1Vc", true},                             // AddressScriptHash
+		{"tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7", true},  // AddressWitnessScriptHash
+		{"2Mx7sVozbZZXPiqsTRWLnZ7bC7vGUEEwX6g", true},                             // legacy - AddressScriptHash
+		{"2MuiiTCHGtQ3MMFhAQ3kFGsJ6N9K89itPcw", true},                             // p2sh-segwit -- AddressScriptHash
+		{"tb1qwtlr3cmn0kg3h6passf7wktmy7596p7swpmxdz6nsp6pmvhzg3eq93qvfz", true},  // bech32 -- AddressWitnessScriptHash
 		{"bc1qpx5p30dcfxqpz5sxemv30ky34lf20jwe7nl95exqcphjvxxehalql9mmrd", false}, // bech32 -- AddressWitnessScriptHash - mainnet
 	}
 
@@ -257,4 +266,147 @@ func TestBTCTestnetAddress(t *testing.T) {
 	checkResult:
 		assert.Equal(t, tc.isValid, actualResult)
 	}
+}
+
+func TestBTCMultiSigRawTx(t *testing.T) {
+	btcPrivateKey1 := "cMp1fNCAXg59DTehiGAJKKsR2ydVdoJXr5z1Yjg4RMGgkbvWUj3d"
+	btcPrivateKey2 := "cRYGyQfT7pqNn6C674CBaCLREkzeE8qLH4ZRQ15vuruSSTWDa1Ar"
+	btcPrivateKey3 := "cSucG9KK6QAXdEoQCQoX64GkRaQXUnvtzbf2ahHuHm2GMqBogxHo"
+	redeemScript, bitcoinKeys, err := BuildMultiSigP2SHAddr([]string{btcPrivateKey1, btcPrivateKey2, btcPrivateKey3}, 2)
+	require.Equal(t, err, nil)
+	multiAddress := btcutil.Hash160(redeemScript)
+
+	// if using Bitcoin main net then pass &chaincfg.MainNetParams as second argument
+	addr, err := btcutil.NewAddressScriptHashFromHash(multiAddress, &chaincfg.TestNet3Params)
+	require.Equal(t, err, nil)
+	fmt.Println(addr)
+	utxos := []*Outputs{
+		{
+			txHash: "3731f9dbc2d0c6b8c0a681e6541b77830c6839bb9bfb15818bd5fab0c5c91bfd",
+			index:  0,
+		},
+		{
+			txHash: "3731f9dbc2d0c6b8c0a681e6541b77830c6839bb9bfb15818bd5fab0c5c91bfd",
+			index:  1,
+		},
+		{
+			txHash: "3731f9dbc2d0c6b8c0a681e6541b77830c6839bb9bfb15818bd5fab0c5c91bfd",
+			index:  2,
+		},
+	}
+	recievers := []*Receiver{
+		{
+			to:     "2NEN96Bn2pERFNgyB29A4iFU5egWP6bDKcw",
+			amount: 8000,
+		},
+	}
+
+	hexSignedTx, err := SpendMultiSig(utxos, recievers, bitcoinKeys, redeemScript)
+	require.Equal(t, err, nil)
+	fmt.Println(hexSignedTx)
+}
+
+func BuildMultiSigP2SHAddr(wifPrvStrs []string, required int) ([]byte, []*btcec.PrivateKey, error) {
+	if len(wifPrvStrs) < required || required < 0 {
+		return nil, nil, errors.New("Invalid signature requirment")
+	}
+	bitcoinPrvs := make([]*btcec.PrivateKey, 0)
+	// create redeem script for 2 of 3 multi-sig
+	builder := txscript.NewScriptBuilder()
+	// add the minimum number of needed signatures
+	builder.AddOp(byte(txscript.OP_1 - 1 + required))
+	for _, v := range wifPrvStrs {
+		wif, err := btcutil.DecodeWIF(v)
+		if err != nil {
+			return nil, nil, err
+		}
+		// public key extracted from wif.PrivKey
+		pk := wif.PrivKey.PubKey().SerializeCompressed()
+		// add the 3 public key
+		builder.AddData(pk)
+		bitcoinPrvs = append(bitcoinPrvs, wif.PrivKey)
+	}
+	// add the total number of public keys in the multi-sig screipt
+	builder.AddOp(byte(txscript.OP_1 - 1 + len(wifPrvStrs)))
+	// add the check-multi-sig op-code
+	builder.AddOp(txscript.OP_CHECKMULTISIG)
+	// redeem script is the script program in the format of []byte
+	redeemScript, err := builder.Script()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return redeemScript, bitcoinPrvs, nil
+}
+
+type Receiver struct {
+	amount int64
+	to     string
+}
+
+type Outputs struct {
+	txHash string
+	index  uint32
+}
+
+func SpendMultiSig(utxos []*Outputs, recievers []*Receiver, beaconKeys []*btcec.PrivateKey, redeemScript []byte) (string, error) {
+	// thanks to: https://medium.com/coinmonks
+
+	redeemTx := wire.NewMsgTx(wire.TxVersion)
+	for _, v := range utxos {
+		utxoHash, err := chainhash.NewHashFromStr(v.txHash)
+		if err != nil {
+			return "", nil
+		}
+		outPoint := wire.NewOutPoint(utxoHash, v.index)
+		txIn := wire.NewTxIn(outPoint, nil, nil)
+		redeemTx.AddTxIn(txIn)
+	}
+
+	for _, v := range recievers {
+		// adding the output to tx
+		decodedAddr, err := btcutil.DecodeAddress(v.to, &chaincfg.TestNet3Params)
+		if err != nil {
+			return "", err
+		}
+		destinationAddrByte, err := txscript.PayToAddrScript(decodedAddr)
+		if err != nil {
+			return "", err
+		}
+
+		// adding the destination address and the amount to the transaction
+		redeemTxOut := wire.NewTxOut(v.amount, destinationAddrByte)
+		redeemTx.AddTxOut(redeemTxOut)
+	}
+
+	for i := range redeemTx.TxIn {
+		// signing the tx
+		signature := txscript.NewScriptBuilder()
+		signature.AddOp(txscript.OP_FALSE)
+		for _, v := range beaconKeys {
+			sig, err := txscript.RawTxInSignature(redeemTx, i, redeemScript, txscript.SigHashAll, v)
+			if err != nil {
+				return "", err
+			}
+			signature.AddData(sig)
+		}
+		signature.AddData(redeemScript)
+		signatureScript, err := signature.Script()
+		if err != nil {
+			// Handle the error.
+			return "", err
+		}
+		redeemTx.TxIn[i].SignatureScript = signatureScript
+	}
+
+	var signedTx bytes.Buffer
+	err := redeemTx.Serialize(&signedTx)
+	if err != nil {
+		// Handle the error.
+		return "", err
+	}
+
+	hexSignedTx := hex.EncodeToString(signedTx.Bytes())
+
+	return hexSignedTx, nil
 }
