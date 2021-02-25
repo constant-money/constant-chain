0a5bca38a5d3081fe61e47913a032a4ed3e93626
thanh-inc <thanh.n@incognito.org>
2021-02-25 17:49:33 +0700

    By pass checking tx privacy when metadata is set to converting request

--- transaction/tx_generic/tx_generic_validate.go
+++ transaction/tx_generic/tx_generic_validate.go
@@ -112,7 +112,7 @@ func MdValidateWithBlockChain(tx metadata.Transaction, chainRetriever metadata.C
 func MdValidate(tx metadata.Transaction, hasPrivacy bool, transactionStateDB *statedb.StateDB, bridgeStateDB *statedb.StateDB, shardID byte) (bool, error) {
 	meta := tx.GetMetadata()
 	if meta != nil {
-		if hasPrivacy && tx.GetVersion() <= 1{
+		if hasPrivacy && tx.GetVersion() <= 1 && tx.GetMetadataType() != metadata.ConvertingRequestMeta {
 			return false, errors.New("Metadata can only exist in non-privacy tx")
 		}
 		validateMetadata := meta.ValidateMetadataByItself()
