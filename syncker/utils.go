228b54a348da2b35144436a83727d7f07ec98a9c
0xKumi <dung.q@autonomous.nyc>
2021-03-05 09:56:54 +0700

    allow get proof from best view

--- syncker/utils.go
+++ syncker/utils.go
@@ -32,34 +32,32 @@ func InsertBatchBlock(chain Chain, blocks []common.BlockInterface) (int, error) 
 			break
 		}
 	}
-	epochCommittee := chain.GetLastCommittee()
-	if epochCommittee == nil || len(epochCommittee) == 0 {
-		epochCommittee = chain.GetCommittee()
-	}
+
+	//validate the last block
+	epochCommittee := chain.GetCommittee()
+	validBlockForInsert := sameCommitteeBlock[:]
 	for i := len(sameCommitteeBlock) - 1; i >= 0; i-- {
 		if err := chain.ValidateBlockSignatures(sameCommitteeBlock[i], epochCommittee); err != nil {
-			sameCommitteeBlock = sameCommitteeBlock[:i]
+			validBlockForInsert = sameCommitteeBlock[:i]
 		} else {
 			break
 		}
 	}
 
-	//epochCommittee := chain.GetCommittee()
-	//for i := len(sameCommitteeBlock) - 1; i >= 0; i-- {
-	//	if err := chain.ValidateBlockSignatures(sameCommitteeBlock[i], epochCommittee); err != nil {
-	//		sameCommitteeBlock = sameCommitteeBlock[:i]
-	//	} else {
-	//		break
-	//	}
-	//}
+	batchingValidate := true
+	//if no valid block, this could be a fork chain, try to insert all with full validation
+	if len(validBlockForInsert) == 0 {
+		validBlockForInsert = sameCommitteeBlock[:]
+		batchingValidate = false
+	}
 
-	for i, v := range sameCommitteeBlock {
+	for i, v := range validBlockForInsert {
 		if !chain.CheckExistedBlk(v) {
 			var err error
 			if i == 0 {
 				err = chain.InsertBlk(v, true)
 			} else {
-				err = chain.InsertBlk(v, false)
+				err = chain.InsertBlk(v, batchingValidate == false)
 			}
 			if err != nil {
 				//committeeStr, _ := incognitokey.CommitteeKeyListToString(epochCommittee)
@@ -67,10 +65,8 @@ func InsertBatchBlock(chain Chain, blocks []common.BlockInterface) (int, error) 
 				return 0, err
 			}
 		}
-
 	}
-
-	return len(sameCommitteeBlock), nil
+	return len(validBlockForInsert), nil
 }
 
 //final block
